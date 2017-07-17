29b468813a45c453d4d4cbd7c1286bbbd85d0a4a
Vincent Trumpff <vincent@stoic.com>
2017-07-17 16:39:32 +0200

    packaging

---
+++ arduino-home-library.h
@@ -0,0 +1,9 @@
+#include "src/utils.h"
+#include "src/service.h"
+#include "src/runtime.h"
+
+#include "src/communication_service.h"
+#include "src/wifi_service.h"
+#include "src/configuration_service.h"
+#include "src/info_service.h"
+#include "src/rgb_service.h"
