c7f5606ddc339e9c810a6b0ae188a7d22f413682
Vincent Trumpff <vincent@stoic.com>
2017-07-17 17:13:43 +0200

    fix pate

--- ArduinoHomeLibrary.h
+++
@@ -1,9 +0,0 @@
-#include "utils.h"
-#include "service.h"
-#include "runtime.h"
-
-#include "communication_service.h"
-#include "wifi_service.h"
-#include "configuration_service.h"
-#include "info_service.h"
-#include "rgb_service.h"
