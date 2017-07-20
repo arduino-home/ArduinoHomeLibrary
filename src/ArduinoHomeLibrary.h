89f03f03cbb92a8e284db9472449ab47ede479b9
Vincent Trumpff <vincent@stoic.com>
2017-07-20 20:33:42 +0200

    add dispatcher_service

--- src/ArduinoHomeLibrary.h
+++ src/ArduinoHomeLibrary.h
@@ -4,6 +4,7 @@
 
 #include "communication_service.h"
 #include "configuration_service.h"
+#include "dispatcher_service.h"
 #include "wifi_setup_service.h"
 #include "http_service.h"
 #include "info_service.h"
