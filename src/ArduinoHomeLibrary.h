13d5a52e6965ce1727ac3438b720b1c8d559582e
Vincent Trumpff <vincent@stoic.com>
2017-07-20 16:40:38 +0200

    fix includes

--- src/ArduinoHomeLibrary.h
+++ src/ArduinoHomeLibrary.h
@@ -3,7 +3,8 @@
 #include "runtime.h"
 
 #include "communication_service.h"
-#include "wifi_service.h"
 #include "configuration_service.h"
+#include "wifi_setup_service.h"
+#include "http_service.h"
 #include "info_service.h"
 #include "rgb_service.h"
